a110f5c175052cabd3224d72366ddc8b864d659b
Ankit <yadava9042@gmail.com>
2026-01-26 19:01:24 +0530

    feat: improve video generation page with enhanced UI and controls

--- src/pages/Genetator.tsx
+++ src/pages/Genetator.tsx
@@ -1,6 +1,8 @@
 import React, { useState } from "react";
 import Title from "../components/Title";
 import UploadZone from "../components/UploadZone";
+import { Loader2Icon, RectangleHorizontalIcon, RectangleVerticalIcon, Wand2Icon } from "lucide-react";
+import { PrimaryButton } from "../components/Buttons";
 
 const Genetator = () => {
   const [name, setName] = useState("");
@@ -37,26 +39,125 @@ const Genetator = () => {
               short-form videos and social posts"
         ></Title>
         <div className="flex gap-20 max-sm:flex-col items-start justify-between">
-            <div className="flex flex-col w-full sm:max-w-60 gap-8 mt-8 mb-12">
-          {/* lrft column */}
-          <UploadZone
-            label="Product Image"
-            file={productImage}
-            onClear={() => setProductImage(null)}
-            onChange={(e) => handleFileChange(e, "product")}
-          />
-          <UploadZone
-            label="Model Image"
-            file={modelImage}
-            onClear={() => setModelImage(null)}
-            onChange={(e) => handleFileChange(e, "model")}
-          />
+          <div className="flex flex-col w-full sm:max-w-60 gap-8 mt-8 mb-12">
+            {/* lrft column */}
+            <UploadZone
+              label="Product Image"
+              file={productImage}
+              onClear={() => setProductImage(null)}
+              onChange={(e) => handleFileChange(e, "product")}
+            />
+            <UploadZone
+              label="Model Image"
+              file={modelImage}
+              onClear={() => setModelImage(null)}
+              onChange={(e) => handleFileChange(e, "model")}
+            />
           </div>
 
           {/* right column */}
-          <div>
-            <p>right column</p>
+          <div className="w-full ">
+            <div className="mb-4 text-gray-300">
+              <label htmlFor="name" className="block text-sm mb-4">
+                Project Name
+              </label>
+              <input
+                type="text"
+                id="name"
+                value={name}
+                onChange={(e) => setName(e.target.value)}
+                placeholder="Name your project"
+                required
+                className="w-full bg-white/3 rounded-lg border-2 p-4
+              text-sm border-violet-200/10 focus:border-violet-500/50 
+              outline-none transition-all"
+              />
             </div>
+            <div className="mb-4 text-gray-300">
+              <label htmlFor="productName" className="block text-sm mb-4">
+                productName
+              </label>
+              <input
+                type="text"
+                id="productName"
+                value={productName}
+                onChange={(e) => setProductName(e.target.value)}
+                placeholder="Name your product"
+                required
+                className="w-full bg-white/3 rounded-lg border-2 p-4
+              text-sm border-violet-200/10 focus:border-violet-500/50 
+              outline-none transition-all"
+              />
+            </div>
+
+            <div className="mb-4 text-gray-300">
+              <label
+                htmlFor="productDescription"
+                className="block text-sm mb-4"
+              >
+                productDescription{" "}
+                <span className="text-xs text-violet-400">(optional)</span>
+              </label>
+              <textarea
+                id="productDecription"
+                rows={4}
+                value={productDecription}
+                onChange={(e) => setProductDescription(e.target.value)}
+                placeholder="Enter the description of the product"
+                className="w-full bg-white/3 rounded-lg border-2 p-4 textsm
+              border-violet-200/10 focus:border-violet-500/50 outline-none resize-none transition-all"
+              ></textarea>
+            </div>
+
+            <div className=" mb-4 text-gray-300">
+              <label className=" block text-sm mb-4">Aspect Radio</label>
+              <div className="flex gap-3">
+              <RectangleVerticalIcon  onClick={()=>setAspectRatio('9:16')}
+                className={`p-2.5 size-13 bg-white/6 rounded transition-all ring-2
+                ring-transparent cursor-pointer ${aspectRatio === '9:16' ? 'ring-violet-500/50 bg-white/10' : ''}`}/>
+
+                <RectangleHorizontalIcon  onClick={()=>setAspectRatio('16:9')}
+                className={`p-2.5 size-13 bg-white/6 rounded transition-all ring-2
+                ring-transparent cursor-pointer ${aspectRatio === '16:9' ? 'ring-violet-500/50 bg-white/10' : ''}`}/>
+                </div>
+            </div>
+
+            <div className="mb-4 text-gray-300">
+              <label
+                htmlFor="userPromt"
+                className="block text-sm mb-4"
+              >
+                User Prompt{" "}
+                <span className="text-xs text-violet-400">(optional)</span>
+              </label>
+              <textarea
+                id="userPromt"
+                rows={4}
+                value={userPromt}
+                onChange={(e) => setUserPrompt(e.target.value)}
+                placeholder="Describe how you want  the narration to be.."
+                className="w-full bg-white/3 rounded-lg border-2 p-4 textsm
+              border-violet-200/10 focus:border-violet-500/50 outline-none resize-none transition-all"
+              ></textarea>
+            </div>
+
+          </div>
+
+        </div>
+
+        {/* button */}
+        <div className="flex justify-center mt-10">
+          <PrimaryButton disabled ={isGenerating} className="px-10 py-3
+          rounded-full disabled:opacity-70 disabled:cursor-not-allowed">
+            {isGenerating ? (<>
+            <Loader2Icon className="size-5 animate-spin"/>Generating...
+
+            </>
+          
+          ) : (<>
+          <Wand2Icon className="size-5"/>Genarate Image
+          </>)}
+          </PrimaryButton>
         </div>
       </form>
     </div>
