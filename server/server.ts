885b9026ca319ef0fcb6775404bb69f35ec71185
Ankit <yadava9042@gmail.com>
2026-02-23 14:23:38 +0530

    feat(clerk): setup Clerk webhooks and store user data in Neon database

--- server/server.ts
+++ server/server.ts
@@ -3,12 +3,16 @@ import express, { Request, Response } from 'express';
 import cors from 'cors';
 import 'dotenv/config';
 import { clerkMiddleware } from '@clerk/express'
+import clerkWebhooks from './controllers/clerk.js';
 
 
 const app = express();
 
 // Middleware
 app.use(cors());
+
+app.post('/api/clerk',express.raw({type: 'application/json'}), clerkWebhooks);
+
 app.use(express.json());
 app.use(clerkMiddleware())
 
@@ -19,6 +23,8 @@ const PORT = process.env.PORT || 5000;
 app.get('/', (req: Request, res: Response) => {
     res.send('Server is Live!');
 });
+
+
 
 app.listen(PORT, () => {
     console.log(`Server is running at http://localhost:${PORT}`);
